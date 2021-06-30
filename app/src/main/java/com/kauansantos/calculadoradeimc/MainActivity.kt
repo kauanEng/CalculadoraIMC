b4ac3179f711d67cabd2f2844f48f3592b270b3e
Kauan Santos <kauansantos83@gmail.com>
2021-06-30 13:09:10 -0300

    App Concluido

--- app/src/main/java/com/kauansantos/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/kauansantos/calculadoradeimc/MainActivity.kt
@@ -2,10 +2,78 @@ package com.kauansantos.calculadoradeimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.kauansantos.calculadoradeimc.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bt_calcular = binding.btCalcular
+        val mensagem = binding.mensagem
+
+        bt_calcular.setOnClickListener{
+            val editPeso = binding.editPeso.text.toString()
+            val editAltura = binding.editAltura.text.toString()
+
+            if(editPeso.isEmpty()) {
+                mensagem.setText("Informe seu Peso")
+            }else if (editAltura.isEmpty()) {
+                mensagem.setText("Informe sua Altura")
+            }else {
+                CalculoIMC()
+            }
+        }
+    }
+
+    private fun CalculoIMC() {
+        val pesoId = binding.editPeso
+        val alturaId = binding.editAltura
+
+        val peso = Integer.parseInt(pesoId.text.toString())
+        val altura = java.lang.Float.parseFloat(alturaId.text.toString())
+        var resultado = binding.mensagem
+        val imc = peso / (altura * altura)
+
+        val mensagem = when {
+            imc <= 18.5 -> "Peso Baixo"
+            imc <= 24.9 -> "Peso Normal"
+            imc <= 29.9 -> "Sobrepeso"
+            imc <= 34.9 -> "Obesidade (Grau 1)"
+            imc <= 39.9 -> "Obesidade (Grau 2)"
+            else -> "Obesidade Mórbida (Grau 3)"
+        }
+
+        imc.toString()
+        resultado.setText("IMC: $imc \n $mensagem")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.menu_principal, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId){
+            R.id.reset -> {
+                val limparEditPeso = binding.editPeso
+                val limparEditAltura = binding.editAltura
+                val limparMensagem = binding.mensagem
+
+                limparEditPeso.setText("")
+                limparEditAltura.setText("")
+                limparMensagem.setText("")
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
